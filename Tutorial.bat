03d35ccf71080e5e6c78d308708d29e45c51cb0f
Lucashgdhhyy <155470121+Lucashgdhhyy@users.noreply.github.com>
2024-09-03 16:01:29 -0400

    Add files via upload

---
+++ Tutorial.bat
@@ -0,0 +1,10 @@
+@echo off
+firewall.cpl
+@echo.
+@echo Ola siga os meus passos
+@echo na janela que abriu va em Ativar ou desativar firewall
+@echo Em seguida coloque todas as caixinhas desligadas
+@echo Nao e recomendavel baixar arquivos seguindo o processo
+@echo Depois de jogado no servidor va novamente na janela e ative novamente
+@echo.
+pause
